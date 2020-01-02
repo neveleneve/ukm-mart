71e493a3727b0c5304afa26904004583ad782d4d
neveleneve <pandupandu813@gmail.com>
2020-01-02 23:14:28 +0700

    adding font-awesome file. testing FA, cause doesn't know how to add icomoon. LOL. 02/01/2020.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,13 @@
   <div id="apps">
     <nav class="navbar navbar-expand-md bg-primary navbar-dark">
       <a class="navbar-brand" href="#">UKM MART</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseNavbar">
-        <span class="navbar-toggler-icon"></span>        
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapseNavbar"
+      >
+        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="collapseNavbar">
         <ul class="navbar-nav">
@@ -11,7 +16,9 @@
             <a class="nav-link" href="">Home</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="">Product</a>
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href=""
+              >Product</a
+            >
             <div class="dropdown-menu">
               <a class="dropdown-item" href="">Elektronik</a>
               <a class="dropdown-item" href="">Kecantikan</a>
@@ -21,27 +28,30 @@
           <li class="nav-item">
             <a class="nav-link" href="">About</a>
           </li>
-        </ul>        
+        </ul>
       </div>
     </nav>
-    <div class="container-fluid midImage">
+    <div class="container-fluid">
       <div class="row">
-        <div class="col-md-12">
-          <div class="col-md-3">
-            <div class="card">
-              <div class="card-body">
-                <img src="jamtangan.jpg" alt="" height="150" width="150"><br>
-                <a href="">Nama Barang</a>
+        <div class="col-md-2 mb-3">
+          <div class="card middleThing">
+            <div class="card-body">
+              <img class="sizeproduct" src="jamtangan.jpg" alt="" />
+              <a class="card-link" href=""><h5>Nama Barang</h5></a>
+              <h6>Rp 1.000.000</h6>
+            </div>
+            <div class="card-footer">
+              <div class="btn-wrapper text-center d-flex justify-content-between">
+                <a class="btn btn-success d-flex align-items-center fa-shopping-cart"></a>
+                <a class="btn btn-warning d-flex align-items-center fa-heart"></a>
               </div>
-              <div class="card-footer">Harga</div>
             </div>
           </div>
-        </div>
+        </div>        
       </div>
     </div>
-  </div>  
+  </div>
 </template>
 
 <script>
-  
 </script>
