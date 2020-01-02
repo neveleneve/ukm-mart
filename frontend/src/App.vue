3b3d776ed96daf674369ad3e9fe6b045bbf5c977
neveleneve <pandupandu813@gmail.com>
2020-01-02 20:09:35 +0700

    adding bootstrap4 and custom css. 02/01/2020

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="apps">
+    <nav class="navbar navbar-expand-md bg-primary navbar-dark">
+      <a class="navbar-brand" href="#">UKM MART</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseNavbar">
+        <span class="navbar-toggler-icon"></span>        
+      </button>
+      <div class="collapse navbar-collapse" id="collapseNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="">Product</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="">Elektronik</a>
+              <a class="dropdown-item" href="">Kecantikan</a>
+              <a class="dropdown-item" href="">Kerajinan Tangan</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">About</a>
+          </li>
+        </ul>        
+      </div>
+    </nav>
+    <div class="container-fluid midImage">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="jamtangan.jpg" alt="" height="150" width="150"><br>
+                <a href="">Nama Barang</a>
+              </div>
+              <div class="card-footer">Harga</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 0px;
-}
-</style>
---
+++ frontend/src/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <Navbar>
+        <div id="login">            
+            <h2>Tester Login Page</h2>
+        </div>
+    </Navbar>
+    
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue'
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
